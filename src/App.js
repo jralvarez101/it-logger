f26532eefe12d981a2f4aaf3eb2fbf641907cb64
martin <alvarez.martinjr@gmail.com>
2021-09-02 13:05:31 -0700

    created search bar/ create react app/ logs component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, Fragment } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/layout/logs/Logs";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
