96b2a453cdeeb319a974a926dcfa15608b8d4788
martin <alvarez.martinjr@gmail.com>
2021-09-02 15:37:32 -0700

    AddBtn & AddLogModal components

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import LogItem from "./LogItem";
-import Preloader from "../Preloader";
+import Preloader from "../layout/Preloader";
 
 function Logs() {
   const [logs, setLogs] = useState([]);
