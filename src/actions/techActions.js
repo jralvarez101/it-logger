4cbcdf93f7ffde82a1436758cdbd36f86ae4361c
martin <alvarez.martinjr@gmail.com>
2021-09-08 12:54:14 -0700

    delete tech functionality

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -52,6 +52,27 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+// Delete Techs
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
 // Set loading
 export const setLoading = () => {
   return {
